94f82e0d5914b912bb1ac7406a550b970027c317
Indika Eranga <indikaeranga06@gmail.com>
2024-02-20 14:06:35 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,55 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hi, Welcome to calculator.");
+
+        }
+
+    public int addition(int a ,int b){
+        return a+b;
+    }
+    public int substration(int a,int b){
+        return a-b;
+    }
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+    public int minimum(int a, int b) {
+        return Math.min(a, b); // Minimum of a and b
+    }
+    public int maximum(int a, int b) {
+        return Math.max(a, b); // Maximum of a and b
+    }
+    public int division(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide number by zero");
+        }
+        return a / b; // Division of a by b
+    }
+    public int modulus(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Modulus by zero is undefined");
+        }
+        return a % b; // Remainder of a divided by b
+    }
+    public int power(int base, int exponent) {
+        return (int) Math.pow(base, exponent); // power value
+    }
+    public int factorial(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("Factorial is undefined for negative numbers");
+        }
+        int result = 1;
+        for (int i = 2; i <= num; i++) {
+            result *= i;
+        }
+        return result; //  Factorial number
+    }
+    public int absoluteValue(int num) {
+        return Math.abs(num); // Absolute value of num
+    }
+    }
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,78 @@
+import org.example.Main;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestCalculator {
+
+
+    public Main main;
+    @Test
+    public void testMainConstrctor()
+    {
+        main =new Main();
+
+        assertNotEquals(main,null);
+    }
+
+    @Test
+    public void testadd()
+    {
+        main=new Main();
+        assertEquals(4,main.addition(2,2));
+    }
+    @Test
+    public void testSubstration()
+    {
+        main=new Main();
+        assertEquals(0,main.substration(2,2));
+    }
+    @Test
+    public void testmultiply()
+    {
+        main=new Main();
+        assertEquals(6,main.multiply(3,2));
+    }
+    @Test
+    public void testminimum()
+    {
+        main=new Main();
+        assertEquals(3,main.minimum(8,3));
+    }
+    @Test
+    public void testmaximum()
+    {
+        main=new Main();
+        assertEquals(8,main.maximum(8,3));
+    }
+    @Test
+    public void testdivision()
+    {
+        main=new Main();
+        assertEquals(2,main.division(8,4));
+    }
+    @Test
+    public void testmodulus()
+    {
+        main=new Main();
+        assertEquals(3,main.modulus(7,4));
+    }
+    @Test
+    public void testpower()
+    {
+        main=new Main();
+        assertEquals(27,main.power(3,3));
+    }
+    @Test
+    public void testfactorial()
+    {
+        main=new Main();
+        assertEquals(6,main.factorial(3));
+    }
+    @Test
+    public void testabsoluteValue()
+    {
+        main=new Main();
+        assertEquals(7,main.absoluteValue(7));
+    }
+}
